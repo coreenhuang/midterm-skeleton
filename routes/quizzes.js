b41106fa68c1a52a861d68efde6a3c633726a7b4
Coreen Huang <coreen.huang@gmail.com>
2022-09-26 16:19:22 -0400

    add form for quizzes

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -5,4 +5,18 @@ router.get('/', (req, res) => {
   res.send('quizzes view goes here');
 });
 
+router.get('/new', (req, res) => {
+  // res.send('new quizzes view goes here');
+  res.render('quizzes/new');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('specific quizzes view goes here');
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Send OK');
+});
+
 module.exports = router;
